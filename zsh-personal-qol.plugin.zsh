e79e3d122a014dc3a4b166834bf58ee240251e2c
Rik Harink <Rik.Harink@topicus.nl>
2023-11-28 10:39:16 +0100

    remove duplicate code

--- zsh-personal-qol.plugin.zsh
+++ zsh-personal-qol.plugin.zsh
@@ -34,24 +34,6 @@ repo() {
         cloud)
             pushd $HOME/repos/topicus/cloud
             ;;
-        personal)
-            pushd $HOME/repos/personal
-            ;;
-        tools)
-            pushd $HOME/repos/tools
-            ;;
-        websites)
-            pushd $HOME/repos/websites
-            ;;
-        sandbox)
-            pushd $HOME/repos/topicus/cloud/aws-sandbox
-            ;;
-        modules)
-            pushd $HOME/repos/topicus/cloud/aws-modules
-            ;;
-        landingzone)
-            pushd $HOME/repos/topicus/cloud/aws-landing-zone
-            ;;
         *)
             pushd $HOME/repos/$1
             ;;
